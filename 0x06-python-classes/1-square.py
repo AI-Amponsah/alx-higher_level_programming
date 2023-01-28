4c2f632ae2dc6846133276abf05d9aef6368ab59
Isaac Amponsah <Yawamp27@gmail.com>
2023-01-28 04:46:26 +0000

    square

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Define class Square"""
+
+
+class Square:
+""" A class Square"""
+
+    def __init__(self, size)i:
+    """Initialize a square
+
+    Aregument:
+    int(size): Represents size of square
+               """
+        self.__size = size
