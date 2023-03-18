73e40fb579efe43f5d1c5416e6a0966e502b50e1
AI-Amponsah <yawamp27@gmail.com>
2023-03-18 02:48:43 +0000

    id

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,5 +1,5 @@
 -- script that lists all cities contained in the database hbtn_0d_usa
 SELECT cities.id, cities.name, states.name
 FROM cities
-INNER JOIN states
-ON cities.states_id = states.id ORDER BY cities.id;
+INNER JOIN state
+ON cities.state_id = state.id ORDER BY cities.id;
