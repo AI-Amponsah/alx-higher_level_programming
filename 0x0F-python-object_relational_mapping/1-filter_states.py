cbefb0c4e2ecb1e4f792b1541494b70955922997
AI-Amponsah <yawamp27@gmail.com>
2023-05-02 15:34:55 +0000

    let's seea

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+""" This scripts prints states from a DB"""
+import MySQLdb
+
+from sys import argv
+if __name__ == "__main__":
+    con = MySQLdb.connect(user=argv[1], passwd=argv[2], port=3306, db=argv[3])
+    cursor = con.cursor()
+    cursor.execute("SELECT * FROM states  WHERE name LIKE 'N%'")
+    states = cursor.fetchall()
+    for state in states:
+        print(state)
+    cursor.close()
+    con.close()
