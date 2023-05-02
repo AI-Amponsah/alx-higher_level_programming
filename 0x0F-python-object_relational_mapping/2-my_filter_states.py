b2d1941c10041754f1da7dd9e3114d6e331fc2d5
AI-Amponsah <yawamp27@gmail.com>
2023-05-02 15:55:00 +0000

    arizona

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+""" This scripts prints states from a DB"""
+import MySQLdb
+
+from sys import argv
+if __name__ == "__main__":
+    con = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3])
+    cursor = con.cursor()
+    cursor.execute("SELECT * FROM states  WHERE BINARY name = '{}'".format(argv[4]))
+    states = cursor.fetchall()
+    for state in states:
+        print(state)
+    cursor.close()
+    con.close()
