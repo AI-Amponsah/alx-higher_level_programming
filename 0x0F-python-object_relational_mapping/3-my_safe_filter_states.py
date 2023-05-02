2d56beadb2ee92fbae7b8a819c3d27a3ed91c8a5
AI-Amponsah <yawamp27@gmail.com>
2023-05-02 16:32:07 +0000

    query

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+""" This scripts prints states from a DB"""
+import MySQLdb
+
+from sys import argv
+if __name__ == "__main__":
+    con = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3])
+    cursor = con.cursor()
+    search = "SELECT * FROM states  WHERE BINARY name = %s"
+    cursor.execute(search, argv[4])
+    states = cursor.fetchall()
+    for state in states:
+        print(state)
+    cursor.close()
+    con.close()
