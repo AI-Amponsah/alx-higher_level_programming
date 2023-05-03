2a4fddec52faf6a14dab1909520bba165b277b97
AI-Amponsah <yawamp27@gmail.com>
2023-05-03 08:58:32 +0000

    real

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-"""  function that reads a text file (UTF8) and prints it to stdout:"""
+""" Module to create a function: read_file """
 
 
 def read_file(filename=""):
-"""Reading a file"""
+    """ reads a text file (UTF8) and prints it to stdout """
 
     with open(filename, encoding="utf-8") as f:
         print(f.read(), end='')
