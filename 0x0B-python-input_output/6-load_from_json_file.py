26591d27fbf0bcd9e11cfbd9491f642ccd706300
AI-Amponsah <yawamp27@gmail.com>
2023-05-03 16:01:04 +0000

    yeah

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -8,5 +8,5 @@ import json
 def load_from_json_file(filename):
     """creates obj from  json"""
 
-    with open(filename, 'w', encoding='utf-8') as f:
+    with open(filename, encoding='utf-8') as f:
         return json.loads(f.read())
